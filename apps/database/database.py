18ecf655714db23163b98f39add9a17409493d8c
sxeks <rsxek06@gmail.com>
2026-06-15 09:38:12 +0300

    first commit, test

--- apps/database/database.py
+++ apps/database/database.py
@@ -1,9 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from .config import database_config
+from time import sleep
 
-engine = create_engine(database_config.DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# postgres не успевает инициализироваться, поэтому оставляем
+# несколько попыток с задержкой в секунду
+
+counter = 10
+while True:
+    counter -= 1
+    try:
+        engine = create_engine(database_config.DATABASE_URL)
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        break
+    except:
+        sleep(1)
+
 
 def get_db() -> Session:
     db = SessionLocal()
